57e9a984287da31c84045f1ad351b88ef65b9c1a
nirmiller <52181530+nirmiller@users.noreply.github.com>
2019-11-24 21:18:09 -0600

    auto movements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/NewAuto/Motor_Power_Spline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/NewAuto/Motor_Power_Spline.java
@@ -5,7 +5,7 @@ public class Motor_Power_Spline {
     double rightPower;
     public static double noLoadSpeed = 31.4 ; // Max Angular Velocity in radians/second for 20 : 1 motor
     public static double stallTorque = 2.1; // Max Torque in Newton Meters for 20 : 1 motor
-    public static double robotlength = 17.75;
+    public static double robotlength = 17.5;
 
 
     @Override
@@ -21,23 +21,25 @@ public class Motor_Power_Spline {
         return rightPower;
     }
 
-    public static double aungular_velocity( double dX, double dY, double sX, double sY)
+    public static double aungular_velocity(double dY, double sY)
     {
-        return (1/(1+Math.pow(dY/1, 2))) * ((sY * dX) / (Math.pow(1, 2)));
+        return (1/(1 + dY * dY)) * sY;
     }
 
     public static double setLeftPower(double aungularVelocity) {
 
 
-        double leftVelocity = 1 - robotlength * aungularVelocity / 2;
+        double leftVelocity = 1 - (robotlength * aungularVelocity / 2);
         return (-1) * (stallTorque / noLoadSpeed) * leftVelocity
                 + stallTorque * leftVelocity;
+        //return leftVelocity;
     }
 
     public static double setRightPower(double aungularVelocity) {
-        double rightVelocity = 1 + robotlength * aungularVelocity / 2;
+        double rightVelocity = 1 + (robotlength * aungularVelocity / 2);
         return (-1) * (stallTorque / noLoadSpeed) * rightVelocity
                 + stallTorque * rightVelocity;
+        //return rightVelocity;
     }
 
     public Motor_Power_Spline(double leftPower, double rightPower) {
